f1faa871b93ac71b19e6bb387e9c6c930d93512e
diyarayaroth <premjith.diya@gmail.com>
2024-05-06 12:47:55 +0530

    add color file
    color file

--- lib/utils/theme_utils.dart
+++ lib/utils/theme_utils.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:health_care/utils/create_material_color.dart';
 
 import 'app_color.dart';
-import 'create_material_color.dart';
 
 class ThemeUtils {
   static ThemeData get lightTheme {
